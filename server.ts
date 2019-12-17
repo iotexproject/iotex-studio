b3dc723df7e550e5d00fd0d3be8d169d3bada09a
koishi <ncysatnaf@gmail.com>
2019-12-17 15:58:16 +0800

    feat: add gzip support

--- server.ts
+++ server.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import serveStatic from "serve-static";
 import path from "path";
+import compression from "compression";
 const app = express();
-app.use(serveStatic(__dirname + "/dist", { maxAge: 86400 * 1000 }));
+app.use(serveStatic(__dirname + "/dist", { maxAge: 86400 * 1000 })).use(compression());
 
 var port = process.env.PORT || 5000;
 app.listen(port);
