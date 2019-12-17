e74e8e3ac27dba68ab7df0d50b812c7cea8f1c60
koishi <ncysatnaf@gmail.com>
2019-12-17 16:05:19 +0800

    fix: compression

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import serveStatic from "serve-static";
 import path from "path";
 import compression from "compression";
 const app = express();
-app.use(serveStatic(__dirname + "/dist", { maxAge: 86400 * 1000 })).use(compression());
+app.use(compression()).use(serveStatic(__dirname + "/dist", { maxAge: 86400 * 1000 }));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
