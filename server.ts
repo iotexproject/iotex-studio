ee125f82fa95ba1a679a1ead4f3884278893e650
koishi <ncysatnaf@gmail.com>
2019-12-17 15:41:10 +0800

    feat: add max-age to static files

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import serveStatic from "serve-static";
 import path from "path";
 const app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + "/dist", { maxAge: 96400 }));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
