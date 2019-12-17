bd17319674db3f871733b46c073fbc45137ea8ed
koishi <ncysatnaf@gmail.com>
2019-12-17 15:51:28 +0800

    fix: max-age

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import serveStatic from "serve-static";
 import path from "path";
 const app = express();
-app.use(serveStatic(__dirname + "/dist", { maxAge: 96400 }));
+app.use(serveStatic(__dirname + "/dist", { maxAge: 86400 * 1000 }));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
