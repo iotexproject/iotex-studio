55a50dee602bd567a6e78b1535cfbd964e67abd5
uuz <ncysatnaf@gmail.com>
2020-06-29 19:36:10 +0800

    deploy provider support

--- src/utils/ws-plugin.ts
+++ src/utils/ws-plugin.ts
@@ -73,6 +73,16 @@ export class WsSignerPlugin implements SignerPlugin {
     return res;
   }
 
+  public async readContract() {
+    await this.wait();
+    const req = {
+      type: "QUERY_PARAMS",
+      payload: { contractAddress: "io1xpvzahnl4h46f9ea6u03ec2hkusrzu020th8xx", method: "totalSupply" },
+    };
+    const res = await this.ws.sendRequest(req);
+    return res.accounts;
+  }
+
   public async getAccount(address: string): Promise<Account> {
     const acct = new Account();
     acct.address = address;
